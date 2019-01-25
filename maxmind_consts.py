a221a814334850088d101ada757b0ff46c0ec6e6
pratikp <pratikp@splunk.com>
2019-01-25 17:28:49 +0530

    Resolved documetation issues

--- maxmind_consts.py
+++ maxmind_consts.py
@@ -1,16 +1,8 @@
-# --
 # File: maxmind_consts.py
+# Copyright (c) 2016-2019 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2014-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 
 # Status/Progress Messages
